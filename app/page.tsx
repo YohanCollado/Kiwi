284bf6dba3f4bc71c3266eb315b84f5e2556a928
YohanCollado <yohancollado@yahoo.com>
2026-02-03 22:39:32 -0500

    add Next.js app files

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className = "text-3xl">
+        Store Kiwi - The Best Storage Solution You Can Find.
+      </h1>
+    </div> 
+  );
+}
