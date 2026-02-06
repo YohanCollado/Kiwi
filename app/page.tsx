919831d8e70d33c798e56652d5a11d3e0a9ee9b0
YohanCollado <yohancollado@yahoo.com>
2026-02-05 23:49:32 -0500

    Set up the whole tech stack and added some simple features to main

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1 className = "text-3xl">
-        Store Kiwi - The Best Storage Solution You Can Find.
+      <h1 className = "text-3xl text-green-400">
+        Kiwi - The Best Storage Solution You Can Find.
       </h1>
+      <button className = "px-4 py-2 rounded-md bg-brand text-red-400">
+        Upload A File
+        </button>
     </div> 
   );
 }
