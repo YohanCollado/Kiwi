9e59a7512a5a8edbb66cd9df0401f691886ec9fd
Yohan Pablo Collado Suri <yohancollado@yahoo.com>
2026-02-19 23:47:10 -0500

    Register Page join now directs to the dashboard page

--- app/authentication/register/RegisterPageNow.tsx
+++ app/authentication/register/RegisterPageNow.tsx
@@ -6,11 +6,12 @@ import {useState} from "react";
 import TopBar from "../../components/TopBar_now"
 import InputField from "@/app/components/ui/InputField";
 import { RegisterErrors } from "@/app/types/register";
+import { useRouter } from "next/navigation";
 
 
 export default function RegisterPage() {
-    const [firstName, setFirstName] = useState<string>("");
-    const [lastName, setLastName] = useState<string>("");
+    const router = useRouter();
+
     const [email, setEmail] = useState<string>("");
     const [password, setPassword] = useState<string>("");
     const [confirmPassword, setConfirmPassword] = useState<string>("");
@@ -33,13 +34,7 @@ export default function RegisterPage() {
             password:"",
             confirmPassword:"",
         };
-
-        if(!firstName.trim()) {
-            newError.firstName="First name is required";
-        }
-        if(!lastName.trim()) {
-            newError.lastName="Last name is required";
-        }
+        
         if(!email.trim()) {
             newError.email="Email is required";
         } else if (!/^\S+@\S+\.\S+$/.test(email)){
@@ -57,8 +52,7 @@ export default function RegisterPage() {
         }
 
         const hasError = Object.values(newError).some(
-            (errorMessage) => errorMessage !== ""
-        );
+            (errorMessage) => errorMessage !== "");
 
         if (hasError){
             setError(newError);
@@ -66,7 +60,10 @@ export default function RegisterPage() {
         }
         setError(newError);
 
-        console.log("Form is valid")
+        if(!hasError){
+            console.log("Form is valid")
+            router.push("/dashboard")
+        }   
     }
 
     return (
@@ -75,28 +72,6 @@ export default function RegisterPage() {
             <h1 className="flex justify-center pt-20 text-6xl text-white text-center">Register Now</h1>
 
             <form onSubmit={handleSubmit} className="flex flex-col items-center gap-1 mt-10">
-                <label className="text-white text-3xl">First Name</label>
-                <InputField
-                    type="text"
-                    placeholder="First Name"
-                    value={firstName}
-                    onChange={(e) =>setFirstName(e.target.value)}
-                    className="w-100 mx-auto"
-                    required
-                    error={error.firstName}
-                    ></InputField>
-
-                <label className="text-white text-3xl">Last Name</label>
-                <InputField
-                    type="text"
-                    placeholder="Last Name"
-                    value={lastName}
-                    onChange={(e) => setLastName(e.target.value)}
-                    className="w-100 mx-auto"
-                    required
-                    error={error.lastName}
-                    ></InputField>
-       
 
                 <label className="text-white text-3xl">Email</label>
                 <InputField
@@ -131,7 +106,8 @@ export default function RegisterPage() {
                     error={error.confirmPassword}
                 ></InputField>
 
-                <button type = "submit" 
+                <button 
+                    type="submit" 
                     className ={`
                     mt-6 px-2 py-2 rounded-md bg-brand
                     text-white border-2 border-green-500
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+//dashboard
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-[#000000] relative">
+        Hello Dashboard
+        </div>
+    
+  );
+}
